b4b06dc6b11030af3a82cb12c59559e39a574028
Sarah Mukuti <cesarwrites24@gmail.com>
2022-09-04 14:00:55 +0300

    create articles migration

--- db/migrate/20220904105344_create_articles.rb
+++ db/migrate/20220904105344_create_articles.rb
@@ -1,4 +1,11 @@
 class CreateArticles < ActiveRecord::Migration[7.0]
   def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :author
+      t.string :image_url
+      t.string :description
+      t.timestamps
+    end
   end
 end
